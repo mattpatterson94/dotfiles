e88d3818b12cccdf3f8fed3171158cf248539eb0
Matt Patterson <matt.patterson94@gmail.com>
2019-06-17 14:26:08 +1000

    Better history support, remove autosuggestions

--- .zsh/load.zsh
+++ .zsh/load.zsh
@@ -2,9 +2,6 @@
 export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 
-# AVN
-[[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
-
 # ASDF
 export ASDF_DIR=$(brew --prefix asdf)
 source $ASDF_DIR/asdf.sh
@@ -13,7 +10,6 @@ source $ASDF_DIR/etc/bash_completion.d/asdf.bash
 # Chruby
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 source /usr/local/opt/chruby/share/chruby/auto.sh
-chruby 2.6.3
 
 # Heroku
 HEROKU_AC_ZSH_SETUP_PATH=~/Library/Caches/heroku/autocomplete/zsh_setup 
--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,6 +1,6 @@
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-autosuggestions"
+#zplug "zsh-users/zsh-autosuggestions"
 
 #zsh-completions
 fpath=(/usr/local/share/zsh-completions $fpath)
