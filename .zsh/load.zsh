b1b1cc44a465c1d21469aed881431e8642d34eef
Matt Patterson <matt.patterson94@gmail.com>
2019-06-17 13:26:49 +1000

    Add more comments to be readable

--- .zsh/load.zsh
+++ .zsh/load.zsh
@@ -1,16 +1,25 @@
-export ASDF_DIR=$(brew --prefix asdf)
+# Z-Plug
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+# AVN
 [[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+
+# ASDF
+export ASDF_DIR=$(brew --prefix asdf)
 source $ASDF_DIR/asdf.sh
 source $ASDF_DIR/etc/bash_completion.d/asdf.bash
 
+# Chruby
+source /usr/local/opt/chruby/share/chruby/chruby.sh
+source /usr/local/opt/chruby/share/chruby/auto.sh
 chruby 2.6.3
 
-HEROKU_AC_ZSH_SETUP_PATH=/Users/mattpatterson/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
+# Heroku
+HEROKU_AC_ZSH_SETUP_PATH=~/Library/Caches/heroku/autocomplete/zsh_setup 
+test -f $HEROKU_AC_ZSH_SETUP_PATH 
+source $HEROKU_AC_ZSH_SETUP_PATH
 
-zstyle :omz:plugins:ssh-agent identities id_rsa id_rsa_envato
-
+# Direnv
 eval "$(direnv hook zsh)"
-
 
