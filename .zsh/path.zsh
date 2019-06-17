3cb1c5257344f458a8973f2734fdefdbee3a960c
Matt Patterson <matt.patterson94@gmail.com>
2019-06-17 17:25:08 +1000

    Revert back to oh-my-zsh

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,6 +1,6 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export PATH="$JAVA_HOME/bin:$PATH"
-export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
 export PATH="/usr/local/opt/mysql@5.6/bin:$PATH"
 export PATH="$HOME/.dotfiles/bin:$PATH"
 
--- .zshrc
+++ .zshrc
@@ -1,7 +1,7 @@
-#zmodload zsh/zprof # Load zprof 
+#zmodload zsh/zprof # Load zprof
 source ~/.zsh/settings.zsh
-source ~/.zsh/load.zsh
 source ~/.zsh/plugins.zsh
+source ~/.zsh/load.zsh
 source ~/.zsh/env.zsh
 source ~/.zsh/aliases.zsh
 source ~/.zsh/secrets.zsh
