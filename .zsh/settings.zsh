b3f98b1fd8275f18e1f6bd06318c26553cdd2206
Matt Patterson <matt.patterson94@gmail.com>
2019-06-17 17:44:49 +1000

    Add some more plugins

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -10,4 +10,5 @@ DISABLE_UPDATE_PROMPT="true"
 # Styles
 ## Ssh Agent autoload identities
 zstyle :omz:plugins:ssh-agent identities id_rsa id_rsa_envato
-
+zstyle :omz:plugins:chruby path /usr/local/opt/chruby/share/chruby/chruby.sh
+zstyle :omz:plugins:chruby auto /usr/local/opt/chruby/share/chruby/auto.sh
