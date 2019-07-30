44c0421f35fe98fdc0c5857a9e51355e9ae60067
Matt Patterson <matt.patterson94@gmail.com>
2019-07-30 12:09:39 +1000

    Updates to ZSH config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,4 +10,4 @@ alias r-install='ruby-install ruby $(< .ruby-version) -- --disable-install-rdoc'
 alias java8='export JAVA_HOME=$JAVA_8_HOME'
 alias java10='export JAVA_HOME=$JAVA_10_HOME'
 alias ssh-remove='ssh-keygen -R'
-
+alias rubylatest='asdf shell ruby 2.6.3'
