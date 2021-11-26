237eaf8b1a312ddd36a08f00d85c7d136f7eb58b
Matt Patterson <matt.patterson@hey.com>
2021-11-26 18:19:07 +1100

    Remove redundant aliases and add colima-start

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,15 +7,12 @@ alias ber="bundle exec rails"
 alias ggfp="git push --force-with-lease origin head"
 alias dc="docker-compose"
 alias crab="docker-compose run app bundle exec"
-alias coppa="git status --porcelain | cut -c4- | grep -E '(\.rb)|(\.rake)' | xargs bundle exec rubocop"
-alias java8='export JAVA_HOME=$JAVA_8_HOME'
-alias java10='export JAVA_HOME=$JAVA_10_HOME'
 alias ssh-remove='ssh-keygen -R'
 alias stacksup='bundle exec stack_master apply'
 alias stacksout='bundle exec stack_master outputs'
 alias perf="curl -w \"@$HOME/.dotfiles/curl-format.txt\" -o /dev/null -s "
 alias subl='open -a "Sublime Text"'
-
+alias colima-start="colima start --mount $HOME/Code:w"
 alias docker-up='docker-compose -f ~/Code/docker/docker-compose.yml up -d'
 alias docker-down='docker-compose -f ~/Code/docker/docker-compose.yml down' 
 
