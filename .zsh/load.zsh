3e3ce51f924d50aa226d8d054a0fa66741e47921
Matt Patterson <matt.patterson@hey.com>
2022-08-29 13:36:49 +1000

    Remove kubectl. Comment out java and android sdk

--- .zsh/load.zsh
+++ .zsh/load.zsh
@@ -11,12 +11,7 @@ export ASDF_HOME=$(brew --prefix asdf)
 # Direnv
 eval "$(direnv hook zsh)"
 
-[[ /usr/local/bin/kubectl ]] && source <(kubectl completion zsh)
-
 complete -C '/opt/homebrew/bin/aws_completer' aws
 
-export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
-export PUPPETEER_EXECUTABLE_PATH=`which chromium`
-
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export ANDROID_SDK_ROOT="$(brew --prefix)/share/android-commandlinetools"
+# export JAVA_HOME="$(/usr/libexec/java_home)"
+# export ANDROID_SDK_ROOT="$(brew --prefix)/share/android-commandlinetools"
