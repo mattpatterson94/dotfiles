1133615115e2891cc9a6fbfa574f521e06592716
Matt Patterson <mattpatterson@canva.com>
2025-01-02 17:31:09 +1000

    clean up aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,17 +7,7 @@ alias be="bundle exec"
 alias ber="bundle exec rails"
 alias ggfp="git push --force-with-lease origin head"
 alias dc="docker-compose"
-alias crab="docker-compose run app bundle exec"
 alias ssh-remove='ssh-keygen -R'
-alias stacksup='bundle exec stack_master apply'
-alias stacksout='bundle exec stack_master outputs'
-alias perf="curl -w \"@$HOME/.dotfiles/curl-format.txt\" -o /dev/null -s "
-alias subl='open -a "Sublime Text"'
-alias colima-start="colima start --mount $HOME/Code:w && fix-docker-sock"
-alias docker-up='docker-compose -f ~/Code/docker/docker-compose.yml up -d'
-alias docker-down='docker-compose -f ~/Code/docker/docker-compose.yml down'
-alias fix-docker-sock="sudo ln -s ~/.colima/docker.sock /var/run/docker.sock"
-alias ej='/opt/homebrew/opt/ejson/bin/ejson'
 alias python='/opt/homebrew/bin/python3'
 alias cat="bat"
 
@@ -26,4 +16,12 @@ function erase_history { local HISTSIZE=0; }
 function cleanup {
   docker system prune -a --volumes -f
   xcrun simctl delete unavailable;
+}
+
+function pull {
+  url=$1
+  prefix=":github_2: :pr-open:"
+  title=$(gh pr view $url --json 'title' | jq -r '.title')
+
+  echo "${prefix} *${title}* ${url}" | pbcopy
 }
